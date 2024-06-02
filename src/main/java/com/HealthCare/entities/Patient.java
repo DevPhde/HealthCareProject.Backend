6101466ce3b30da2244b52cf23b4cdcb39cfeb07
Diego B. <dbsmendes@gmail.com>
2024-06-02 17:53:54 -0300

    [FEAT] Implement Patient entity constructor

--- src/main/java/com/HealthCare/entities/Patient.java
+++ src/main/java/com/HealthCare/entities/Patient.java
@@ -38,4 +38,24 @@ public class Patient extends Entity {
     @ManyToOne
     @JoinColumn(name = "Doctor_Id", nullable = false)
     private Doctor DoctorId;
+
+    public Patient(String fullName, String email, String cpf, String password, Date dateBirth, Date lastLogin, Doctor doctor, Date createdAt, Date updatedAt, Boolean isActive) {
+        FullName = fullName;
+        Email = email;
+        Cpf = cpf;
+        Password = password;
+        DateBirth = dateBirth;
+        LastLogin = lastLogin;
+        DoctorId = doctor;
+
+
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt;
+        IsActive = isActive;
+    }
+
+    public Patient(Integer id ,String fullName, String email, String cpf, String password, Date dateBirth, Date lastLogin, Doctor doctor, Date createdAt, Date updatedAt, Boolean isActive) {
+        this(fullName, email, cpf, password, dateBirth, lastLogin, doctor, createdAt, updatedAt, isActive);
+        Id = id;
+    }
 }
